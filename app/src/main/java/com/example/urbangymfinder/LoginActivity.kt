a83af2e4739e884a97a52f6ad5761ef79948d887
Natalia <nserarfrancino@gmail.com>
2020-03-20 15:00:31 +0100

    login almost finished button onclick workinggg

--- app/src/main/java/com/example/urbangymfinder/LoginActivity.kt
+++ app/src/main/java/com/example/urbangymfinder/LoginActivity.kt
@@ -1,13 +1,35 @@
 package com.example.urbangymfinder
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var txtUser:EditText
+    private lateinit var txtPassword:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        txtUser = findViewById(R.id.et_email)
+        txtPassword = findViewById(R.id.et_password)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener{loginUser()}
+
     }
+
+    private fun loginUser(){
+        val user: String = txtUser.text.toString()
+        val password: String = txtUser.text.toString()
+
+        val intent = Intent(this, MainActivity::class.java).also { it.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT) }
+
+        startActivity(intent)
+
+    }
+
 }
