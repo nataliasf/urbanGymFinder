e82d59cc09dd2870214d73fd79857d836793a2b4
Pol <pol@mensakas.com>
2020-03-21 18:27:41 +0100

    loginActivity linked to mainActivity. Basic functions, extras and startActivity(intent)

--- app/src/main/java/com/example/urbangymfinder/LoginActivity.kt
+++ app/src/main/java/com/example/urbangymfinder/LoginActivity.kt
@@ -1,13 +1,32 @@
 package com.example.urbangymfinder
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.*
 
 class LoginActivity : AppCompatActivity() {
 
+    // exemple var nom usuari del login per pasar a seguent activity
+    var userName: String = "runnerconfinat"
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+
+        //funcio login
+        fun login() {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("userName", userName)
+            startActivity(intent)
+        }
+
+        // funcio on click button
+        findViewById<Button>(R.id.logInBtn).setOnClickListener {
+            login()
+        }
+
+
+
     }
 }
