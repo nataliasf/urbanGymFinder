bb816b7e41b55783262c83b289cb21288ba57965
Pol <pol@mensakas.com>
2020-03-22 20:28:52 +0100

    main activity map and filter fix

--- app/src/main/java/com/example/urbangymfinder/MainActivity.kt
+++ app/src/main/java/com/example/urbangymfinder/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.Toast
-import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
