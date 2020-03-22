4064ee486c3975cd4d150e3ce994444a04634699
Marc <colominasmarc99@gmail.com>
2020-03-22 18:41:52 +0100

    Conectivitat config perfil map

--- app/src/main/java/com/example/urbangymfinder/ConfigActivity.kt
+++ app/src/main/java/com/example/urbangymfinder/ConfigActivity.kt
@@ -1,13 +1,47 @@
 package com.example.urbangymfinder
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ConfigActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.config_activity);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.config_activity)
+
+
+
+    fun save(){
+        val intent = Intent(this,PerfilActivity::class.java)
+        startActivity(intent)
+
+
+
     }
+    findViewById<Button>(R.id.btnSave).setOnClickListener {
+        save()
+    }
+
+    fun back(){
+            val intent = Intent(this,PerfilActivity::class.java)
+            startActivity(intent)
+
+
+
+    }
+     findViewById<Button>(R.id.btnSave).setOnClickListener {
+            back()
+        }
+
 }
+
+
+}
+
+
+
+
--- app/src/main/java/com/example/urbangymfinder/Mapa.kt
+++ app/src/main/java/com/example/urbangymfinder/Mapa.kt
@@ -1,7 +1,11 @@
 package com.example.urbangymfinder
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.mapa.*
 
 class Mapa : AppCompatActivity() {
 
@@ -9,6 +13,17 @@ class Mapa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mapa);
+
+
+   fun back(){
+       val intent= Intent(this,PerfilActivity::class.java)
+       startActivity(intent)
+   }
+
+    findViewById<Button>(R.id.btnReturn).setOnClickListener {
+
+        back()
     }
 }
 
+}
