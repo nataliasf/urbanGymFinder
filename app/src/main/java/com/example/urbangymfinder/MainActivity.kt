75805f347a4752c46062f30f0759238e74c3603d
Natalia <nserarfrancino@gmail.com>
2020-05-14 19:11:14 +0200

    FireBase connected (database and authentication)

--- app/src/main/java/com/example/urbangymfinder/MainActivity.kt
+++ app/src/main/java/com/example/urbangymfinder/MainActivity.kt
@@ -5,12 +5,24 @@ import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var database: FirebaseDatabase;
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //testing database.
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
 
         // funcions on click btns
         findViewById<ImageButton>(R.id.btnEvents).setOnClickListener {
