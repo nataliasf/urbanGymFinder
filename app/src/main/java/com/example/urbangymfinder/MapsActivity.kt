c71f2029400d742ddd915e6e3b5e1de51571ddc1
Pol <pol@mensakas.com>
2020-05-16 06:23:04 +0200

    map first configuration

--- app/src/main/java/com/example/urbangymfinder/MapsActivity.kt
+++ app/src/main/java/com/example/urbangymfinder/MapsActivity.kt
@@ -1,23 +1,29 @@
 package com.example.urbangymfinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
-    private lateinit var mMap: GoogleMap
+
+    private lateinit var map: GoogleMap
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -33,11 +39,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
+        map.getUiSettings().setZoomControlsEnabled(true)
+        map.setOnMarkerClickListener(this)
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add a marker in Barcelona and move the camera
+        //41°23'11.4"N 2°09'49.3"E
+        val ub = LatLng(41.23, 2.09)
+        map.addMarker(MarkerOptions().position(ub).title("Marker in Barcelona"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(ub))
+
+        //Zoom level 0 corresponds to the fully zoomed-out world view.
+        // Most areas support zoom levels up to 20, while more remote areas
+        // only support zoom levels up to 13.
+        // A zoom level of 12 is a nice in-between value that shows enough
+        // detail without getting crazy-close.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(ub, 12.0f))
     }
+
+    override fun onMarkerClick(p0: Marker?) = false
 }
