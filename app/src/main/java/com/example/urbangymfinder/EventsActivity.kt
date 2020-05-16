84b9fe98233598158ebc7b8be18f0f7f3b6642d0
Pol <pol@mensakas.com>
2020-05-16 18:42:51 +0200

    minor fix to eventsactivity listview to recyclerview change, and layouts missing weigths errors

--- app/src/main/java/com/example/urbangymfinder/EventsActivity.kt
+++ app/src/main/java/com/example/urbangymfinder/EventsActivity.kt
@@ -7,12 +7,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
-import kotlinx.android.synthetic.main.activity_events.*
-import java.io.Serializable
-
-
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.io.Serializable
 
 
 class EventsActivity : AppCompatActivity() {
@@ -101,6 +100,7 @@ class EventsActivity : AppCompatActivity() {
             "descripcio del event, participants, etc..." ))
 
         val EventsAdapter = EventsAdapter(this, eventsList)
+        /*
         lvEvents.adapter = EventsAdapter
         lvEvents.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, id ->
             //TODO NEW ACTIVITY INTENT
@@ -108,6 +108,8 @@ class EventsActivity : AppCompatActivity() {
             //        eventsList[position].name + "event?", Toast.LENGTH_SHORT).show()
             eventDetailActivity(position)
         }
+        
+         */
 
         fun home(){
             val intent = Intent(this,MainActivity::class.java)
