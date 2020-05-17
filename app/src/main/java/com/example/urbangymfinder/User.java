8d03ddc5bac27169251bd946fdfdedcf1cf0da8e
danielruizbasas <32066166+danielruizbasas@users.noreply.github.com>
2020-05-17 19:49:37 +0200

    Añadir a favoritos parcialmente hecho

--- app/src/main/java/com/example/urbangymfinder/User.java
+++ app/src/main/java/com/example/urbangymfinder/User.java
@@ -1,10 +1,13 @@
 package com.example.urbangymfinder;
 
+import java.util.ArrayList;
+
 public class User {
 
     private String email;
     private String nombre;
     private String password;
+    private ArrayList<Spot> favoritespots;
 
     public User(String name, String email, String password){
         this.nombre = name;
@@ -12,4 +15,39 @@ public class User {
         this.password = password;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<Spot> getFavoritespots() {
+        return favoritespots;
+    }
+
+    public void setFavoritespots(ArrayList<Spot> favoritespots) {
+        this.favoritespots = favoritespots;
+    }
+
+    public void addFavorite(Spot favorite) {
+        this.favoritespots.add(favorite);
+    }
 }
